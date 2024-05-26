5bdd08f264dbaefc2ec6031533d2a81b5009f342
om.gavade <115257951+G-Om@users.noreply.github.com>
2024-05-26 20:02:02 +0530

    Initializing backend for Dashboard

---
+++ Backend/dashboard/src/test/java/ecom/services/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ecom.services.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
